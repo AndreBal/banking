7cf68dee0cf4111f3ce38939b48fa42f7f8199c4
Andrei Balash <whiteswf@gmail.com>
2023-09-04 04:29:00 +0300

    Added docker compose and init.sql

--- src/main/java/com/balash/banking/server/BankingServer.java
+++ src/main/java/com/balash/banking/server/BankingServer.java
@@ -1,61 +1,40 @@
 package com.balash.banking.server;
 
 import com.balash.banking.dao.UserDAO;
+import com.balash.banking.dao.connection.ConnectionProvider;
 import com.balash.banking.service.TransactionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
+import javax.sql.DataSource;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Arrays;
 
 
-
 @Path("/banking")
-public class BankingServer{
+public class BankingServer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BankingServer.class);
     private static final String UNKNOWN_ERROR = "Unnknown error";
 
     private TransactionService transactionService = null;
 
-
-    private TransactionService getTransactionService() throws SQLException {
-        if(transactionService == null){
+    private TransactionService getTransactionService(){
+        if (transactionService == null) {
             transactionService = new TransactionService();
         }
         return transactionService;
     }
 
-/*
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public String getData() {
-        try {
-            UserDAO userDao = new UserDAO();
-            AccountDAO accountDAO = new AccountDAO();
-            BankDAO bankDAO = new BankDAO();
-            TransactionDAO transactionDAO = new TransactionDAO();
-            String users = Arrays.toString(userDao.getAllUsers().toArray());
-            String accounts = Arrays.toString(accountDAO.getAllAccounts().toArray());
-            String banks = Arrays.toString(bankDAO.getAllBanks().toArray());
-            String transactions = Arrays.toString(transactionDAO.getAllTransactions().toArray());
-            String data = users + accounts + banks + transactions;
-            LOGGER.debug(data);
-            return data;
-        } catch (Exception e) {
-            LOGGER.error(e.getMessage(), e);
-        }
-        return "Error";
-    }*/
-
     @Path("/transfer/{donor}/{recipient}/{amount}")
     @PUT
-    @Produces(MediaType.TEXT_PLAIN+"; charset=utf-8")
-    public Response transfer(@PathParam("donor") String donor,@PathParam("recipient") String recipient,@PathParam("amount") String amount) {
+    @Produces(MediaType.TEXT_PLAIN + "; charset=utf-8")
+    public Response transfer(@PathParam("donor") String donor, @PathParam("recipient") String recipient, @PathParam("amount") String amount) {
         String result = UNKNOWN_ERROR;
         try {
             TransactionService transactionService = getTransactionService();
@@ -69,8 +48,8 @@ public class BankingServer{
 
     @Path("/deposit/{recipient}/{amount}")
     @PUT
-    @Produces(MediaType.TEXT_PLAIN+"; charset=utf-8")
-    public Response deposit(@PathParam("recipient") String recipient,@PathParam("amount") String amount) {
+    @Produces(MediaType.TEXT_PLAIN + "; charset=utf-8")
+    public Response deposit(@PathParam("recipient") String recipient, @PathParam("amount") String amount) {
         String result = UNKNOWN_ERROR;
         try {
             TransactionService transactionService = getTransactionService();
@@ -84,8 +63,8 @@ public class BankingServer{
 
     @Path("/withdraw/{donor}/{amount}")
     @PUT
-    @Produces(MediaType.TEXT_PLAIN+"; charset=utf-8")
-    public Response withdrawal(@PathParam("donor") String donor,@PathParam("amount") String amount) {
+    @Produces(MediaType.TEXT_PLAIN + "; charset=utf-8")
+    public Response withdrawal(@PathParam("donor") String donor, @PathParam("amount") String amount) {
         String result = UNKNOWN_ERROR;
         try {
             TransactionService transactionService = getTransactionService();
